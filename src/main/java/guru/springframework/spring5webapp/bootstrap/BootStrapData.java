de95ee13ad79d9581088c8506caa634a7b78b7d7
latika.singh <lsingh3@hawk.iit.edu>
2022-01-12 01:52:36 -0500

    Understood and solved Persistent state error for Publisher instance in BootStrapData.java

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring5webapp.bootstrap;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
 import guru.springframework.spring5webapp.domain.Publisher;
@@ -8,6 +9,8 @@ import guru.springframework.spring5webapp.repositories.BookRepository;
 import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
 
 /**
  * Created by jt on 12/23/19.
@@ -30,24 +33,33 @@ public class BootStrapData implements CommandLineRunner {
 
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "123123");
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "3939459459");
+        Book newBook = new Book("Test Book", "3936789");
         Publisher mcGrawHill = new Publisher("TataMcGrawHill","123 William St", "Leesburg", "VA", 20176 );
+        publisherRepository.save(mcGrawHill);
+
+        eric.getBooks().add(newBook);
+        newBook.getAuthors().add(eric);
+        newBook.setPublisher(mcGrawHill);
+        mcGrawHill.getBooks().add(newBook);
+        authorRepository.save(eric);
+        bookRepository.save(newBook);
+        publisherRepository.save(mcGrawHill);
+
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
-        mcGrawHill.getBooks().add(ddd);
         ddd.setPublisher(mcGrawHill);
-
-
+        mcGrawHill.getBooks().add(ddd);
         authorRepository.save(eric);
         bookRepository.save(ddd);
         publisherRepository.save(mcGrawHill);
 
-        Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "3939459459");
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
-        mcGrawHill.getBooks().add(noEJB);
+        noEJB.getAuthors().add(eric);
         noEJB.setPublisher(mcGrawHill);
-
+        mcGrawHill.getBooks().add(noEJB);
         authorRepository.save(rod);
         bookRepository.save(noEJB);
         publisherRepository.save(mcGrawHill);
@@ -55,6 +67,12 @@ public class BootStrapData implements CommandLineRunner {
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books: " + bookRepository.count());
         System.out.println("Number of Publishers: " + publisherRepository.count());
+        System.out.println("Number of Authors: " + authorRepository.count());
         System.out.println("Number of Books for Publisher: " + mcGrawHill.getBooks().size());
+        System.out.println("Publisher of Book DDD is : " + ddd.getPublisher().getName());
+        System.out.println("Publisher of Book NOEJB is : " + noEJB.getPublisher().getName());
+        System.out.println("Publisher_ID of Book NOEJB is : " + noEJB.getPublisher().getId());
+        System.out.println("Authors of book NoEJB are : " + noEJB.getAuthors().stream().map(a->a.getFirstName()).collect(Collectors.toList()));
+        System.out.println("No. of books Eric wrote : " + eric.getBooks().stream().map(b->b.getTitle()).collect(Collectors.toList()));
     }
 }
